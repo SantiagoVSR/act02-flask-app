63374b88fde6c9e0affed937eb66434f71bf220d
SantiagoVSR <73039684+SantiagoVSR@users.noreply.github.com>
2025-04-16 20:30:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-@app.route('/')
+
 def home():
     url = "https://gist.githubusercontent.com/reroes/502d11c95f1f8a17d300ece914464c57/raw/872172ebb60e22e95baf8f50e2472551f49311ff/gistfile1.txt"
     try:
